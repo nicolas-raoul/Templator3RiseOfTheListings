cf78ed258d49d03e98f2409e5440cddc95262475
Nicolas Raoul <nicolas.raoul@gmail.com>
2015-02-05 15:43:23 +0900

    Catch the most obvious

---
+++ find-non-templated-pois.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+#
+# Wikivoyage2OSM
+#
+# Extract Wikivoyage Points Of Interest (POI), validate them, and generate OpenStreetMap (OSM) and CSV files.
+# Reference: https://en.wikivoyage.org/wiki/Wikivoyage:Listings
+# To make URLs clickable in CSV files, search for 'http.*' and replace with '=HYPERLINK("&")' as per https://forum.openoffice.org/en/forum/viewtopic.php?f=9&t=18313#p83972
+#
+# Usage ./wikivoyage.sh enwikivoyage-20131130-pages-articles.xml
+#
+# License: GNU-GPLv3
+# Website: https://github.com/nicolas-raoul/wikivoyage2osm
+# Tracker: https://github.com/nicolas-raoul/wikivoyage2osm/issues
+# Results: https://sourceforge.net/p/wikivoyage
+
+####################################################
+# Settings begin
+####################################################
+
+# Target file (unit test if none given).
+XML=${1:-rattanakosin.xml}
+
+cat $XML |\
+tr '\n' ' ' |\
+awk -vRS='<title>' -vORS='\n<title>' 1 |\
+awk -vRS='</title>' -vORS='\n</title>' 1 |\
+grep --before-context=1 "\*'''" |\
+grep "^<title>" |\
+sed -e "s/<title>/\[\[/;s/$/\]\]/" \
+> out.wikicode
